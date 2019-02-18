445b3cca46c4cb331a463c39698ef75d4ebc937c
Grover, Lalit <lgrover@9801a79e42b3.ant.amazon.com>
2019-02-18 15:26:27 -0500

    Adding botocore and boto3 manually in lambda.zip file

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo 'pip install -r ../source/requirements.txt -t ../source/'
+pip install -r ../source/requirements.txt -t ../source/custom-resource-py/
 
 cd ../source/custom-resource-js/
 npm install --silent

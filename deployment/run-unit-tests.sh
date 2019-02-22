71d7754456316e459e1395a9d8362335d215f226
tomnight <tomnight@amazon.com>
2019-02-22 14:41:04 -0500

    feature/P20903447 remove node_modules and lock file after unit test

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -3,3 +3,7 @@
 cd ../source/custom-resource-js/
 npm install --silent
 npm test
+
+# remove unwanted modules folder so not included in build s3 and open source steps.
+rm -rf node_lodules/
+rm package-lock.json

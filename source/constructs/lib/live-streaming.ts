85d83e9415d940d39b2ce032220cbae1802e228a
SanDim Ciin <ciidim@amazon.com>
2022-04-11 15:17:16 -0700

    initial configuration for dependencies in package.json. added cdk solution helper. updated build script

---
+++ source/constructs/lib/live-streaming.ts
@@ -0,0 +1,30 @@
+/*********************************************************************************************************************
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+ import * as cdk from '@aws-cdk/core';
+ import * as s3 from '@aws-cdk/aws-s3';
+
+
+
+ export class LiveStreaming extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+      /**
+       * CloudFormation Template Descrption
+       */
+      this.templateOptions.description = 'Live Streaming test';
+
+      const bucket = new s3.Bucket(this, 'SanDimTestS3Bucket');
+      
+    }
+  }

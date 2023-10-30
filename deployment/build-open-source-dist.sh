0fbd3b9ce142b95e9a57e0fe07484d7ceb55e2e8
Eddie <eggoynes@amazon.com>
2023-10-29 23:14:06 -0700

    Added one line to s3 distribution script so solution manifest file is now copied to the open source direction when the build pipeline runs

--- deployment/build-open-source-dist.sh
+++ deployment/build-open-source-dist.sh
@@ -48,6 +48,7 @@ cp $source_template_dir/../CODE_OF_CONDUCT.md $dist_dir
 cp $source_template_dir/../CONTRIBUTING.md $dist_dir
 cp $source_template_dir/../CHANGELOG.md $dist_dir
 cp $source_template_dir/../.gitignore $dist_dir
+cp $source_template_dir/../solution-manifest.yaml $dist_dir
 
 echo "------------------------------------------------------------------------------"
 echo "Copying Architecture Diagram and GitHub Templates"
